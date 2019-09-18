3609bf9335afc77c492e11920062cb045538a24e
Dang Tran <hdang.sea@gmail.com>
2019-09-18 00:10:27 -0500

    break word in conversation and add progress for upload s3

--- wp-content/themes/microjobengine/includes/alias-function.php
+++ wp-content/themes/microjobengine/includes/alias-function.php
@@ -1340,7 +1340,7 @@ function mje_render_attach_file_button($container_id, $text = "", $isS3Upload = 
         </span>
     </div>
     <span class="et_ajaxnonce" id="<?php echo wp_create_nonce('ad_carousels_et_uploader'); ?>"></span>
-    <div class="custom-progress" id="custom-order_progress"></div>
+    <div class="custom-progress" id="<?php echo $container_id ?>_progress"></div>
     <?php
 $output = ob_get_clean();
 	echo $output;
